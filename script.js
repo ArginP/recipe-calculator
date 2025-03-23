9344156384c0845cde0a6d4c244154933b1dff60
ArginP <n7toman@gmail.com>
2025-03-23 18:01:58 +0300

    feat: add bootstrap, add initial markup, add dark/light theme switch

---
+++ script.js
@@ -0,0 +1,19 @@
+// --- HTML элементы ---
+const checkbox = document.querySelector('#checkbox');
+const html = document.querySelector('html');
+
+// --- Функция переключения темы ---
+function setTheme(checked) {
+    if (checked) { // выбрана светлая тема
+        html.dataset.bsTheme = 'light';
+    } else { // выбрана темная тема
+        html.dataset.bsTheme = 'dark';
+    }
+}
+
+// --- Обработчик события переключения чекбокса выбора темы ---
+checkbox.addEventListener('change', (e) => {
+    setTheme(e.target.checked);
+})
+
+setTheme(checkbox.checked); // устанавливает тему при изначальной загрузке страницы
